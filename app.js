4d3ba23b7168b08f92d4f2b28d59470df3de3f0c
Abdulrhman Mohammed <61534551+abdulrhmanmohammed198@users.noreply.github.com>
2023-10-22 15:15:01 +0300

    Working on the api

--- app.js
+++ app.js
@@ -1,9 +1,60 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello From server side');
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '/dev-data/data/tours-simple.json'))
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params.id);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  const toursPath = path.join(__dirname, '/dev-data/data/tours-simple.json');
+  fs.writeFile(toursPath, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  });
 });
 
 app.listen(3000, () => {
