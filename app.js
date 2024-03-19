701ae7e4e7c02b61a5caf21ff31164397847b349
Abdulrhman Mohammed <abdohamdy198461@gmail.com>
2024-03-19 23:25:48 +0300

    Add pug package and adding views

--- app.js
+++ app.js
@@ -1,51 +1,52 @@
 const path = require('path');
 const express = require('express');
-
 const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const bodyParser = require('body-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRouts');
+const viewRouter = require('./routes/viewRouts');
 
 const app = express();
 
+// Set view engine and views directory
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware for parsing request bodies
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//? Global  Middlewares:
-// Set Security HTTP headers
+// Security middleware
 app.use(helmet());
 
-// Development logging
+// Development logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Rate limiting middleware
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
-
-// Limit requests from same API
 app.use('/api', limiter);
 
-// Body parser, reading data from body into req.body
+// Data sanitization middleware
 app.use(express.json({ limit: '10kb' }));
-
-// Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
-
-// Data sanitization against XSS
 app.use(xss());
 app.use(
   hpp({
@@ -59,18 +60,19 @@ app.use(
     ],
   })
 );
-// Serving static files
-app.use(express.static(path.join(__dirname, 'public')));
 
-//? Routs:
+// Routes
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
 
+// Error handling for unmatched routes
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+// Global error handling middleware
 app.use(globalErrorHandler);
 
 module.exports = app;
