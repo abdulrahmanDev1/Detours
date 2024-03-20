58bb93154862909076e94dae4963dca6d9f53fc5
Abdulrhman Mohammed <abdohamdy198461@gmail.com>
2024-03-20 05:39:18 +0300

    Testing

--- app.js
+++ app.js
@@ -69,4 +69,25 @@ app.all('*', (req, res, next) => {
 // Global error handling middleware
 app.use(globalErrorHandler);
 
-module.exports = app;
+// module.exports = app;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+mongoose.set('strictQuery', false);
+
+// const app = require('./app');
+
+//? Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  const url = `http://localhost:${port}`;
+  console.log(`Connected on ${url}`);
+});
